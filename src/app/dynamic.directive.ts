9f2ede6df82f3989d72de451a79181ceb2924d4f
CAWilson <charlotteawilson.94@gmail.com>
2023-01-22 19:42:40 +0000

    viewRef example with factory

--- src/app/dynamic.directive.ts
+++
@@ -1,8 +0,0 @@
-import { Directive, ViewContainerRef } from '@angular/core';
-
-@Directive({
-  selector: '[dynamic-directive]'
-})
-export class DynamicDirective {
-  constructor(public viewContainerRef: ViewContainerRef) {}
-}
